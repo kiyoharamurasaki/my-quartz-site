6f4c8b0bcf30b7e52152019393f28df4d4a8b90a
kiyoharamurasaki <kiyohara.murasaki@gmail.com>
2026-02-13 00:43:45 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "kiyoharamurasaki.github.io/my-quartz-site",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
